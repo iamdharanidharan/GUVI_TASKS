b4d66866a523508cfb98953de833f18246f4aac8
Dharanidharan Mani <iamdharanidharan@gmail.com>
2021-01-12 00:47:07 +0530

    Task2 first question

---
+++ Task2/CountriesAPI/script.js
@@ -0,0 +1,12 @@
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://restcountries.eu/rest/v2/all',true);
+
+request.send();
+
+request.onload = function(){
+    let  countryData= JSON.parse(this.response);
+    for(i in countryData)
+    console.log(countryData[i].name);
+    
+}
